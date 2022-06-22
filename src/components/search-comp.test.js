3e3c9f680b979d2f702f7c6069339ccae2eb39ac
fegananca <fegananca@gmail.com>
2022-06-22 20:33:01 +0200

    initial commit

--- src/components/search-comp.test.js
+++ src/components/search-comp.test.js
@@ -1,6 +1,5 @@
-import Search from "./search-comp";
+import { Search } from "./search-comp";
 import { render, screen } from "@testing-library/react";
-import React from "react";
 
 test("search a friend", () => {
   render(<Search />);
